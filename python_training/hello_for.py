72d1c107d1056ffe96826aed376511375a43a918
Rhodey35_Win <4arhodes@gmail.com>
2017-12-06 01:10:53 -0700

    updating python

---
+++ python_training/hello_for.py
@@ -0,0 +1,21 @@
+""" farwell responses """
+
+import random
+
+# twenty different farwells
+goodbye = ['see you', 'laters', 'have a good one', 'goodnight',
+           'going now', 'thanks alot', 'its been real', 'what a night',
+           'to da loo', 'leaving', 'leaving now', 'in the morning',
+           'thats all', 'to the morrow', 'arrg me mate', 'till the sun comes up']
+
+
+for comment in list(goodbye):
+
+        #check friend for goodbye response
+
+        if not random.randint(0,19):
+            print('We shall see you later.')
+            break
+        else:
+            print('parting, {} and have a good one.'.format(comment))
+            goodbye.remove(comment)
---
+++ python_training/hello_try.py
@@ -0,0 +1,14 @@
+""" Try do download a page from the internet when I say hi """
+
+
+import urllib.request
+
+try:
+webpage =  urllib.request.urlopen('http://www.google.com')
+except:
+    print('could not reach website in question')
+else:
+    for line in webpage:
+        print(line)
+
+
